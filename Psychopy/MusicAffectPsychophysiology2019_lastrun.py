0a296f4cf43675b76d888e3eb3d842dc66a217ce
Joel Larwood <joel.larwood@uq.net.au>
2019-05-30 16:57:41 +1000

    All semester 1 testing complete. Data up to here for ISRE

--- Psychopy/MusicAffectPsychophysiology2019_lastrun.py
+++ Psychopy/MusicAffectPsychophysiology2019_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.5),
-    on Mon May 27 15:58:19 2019
+    on Thu May 30 15:57:12 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
