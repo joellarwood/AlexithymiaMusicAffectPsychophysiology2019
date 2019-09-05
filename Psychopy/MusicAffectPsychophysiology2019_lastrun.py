cb0ea1231a2ab61103284781947e568fa584d36a
Joel Larwood <joellarwood@uq.net.au>
2019-09-05 13:17:34 +1000

    sep 5 testing until 1300
    ttl stopped working for particpant 106, session not run

--- Psychopy/MusicAffectPsychophysiology2019_lastrun.py
+++ Psychopy/MusicAffectPsychophysiology2019_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.5),
-    on Wed Sep  4 11:01:11 2019
+    on Thu Sep  5 13:12:40 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
