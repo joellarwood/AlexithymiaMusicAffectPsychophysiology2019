5e92a5adffd07432281d4cfaf3bc2a128907489b
Joel Larwood <joellarwood@uq.net.au>
2019-09-26 18:04:35 +1000

    testing until 26 september

--- Psychopy/MusicAffectPsychophysiology2019_lastrun.py
+++ Psychopy/MusicAffectPsychophysiology2019_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.5),
-    on Mon Sep 23 10:11:20 2019
+    on Thu Sep 26 17:02:22 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
